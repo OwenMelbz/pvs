18027319e11f1924a7fd5939bf10f2ff5a3d9dfc
Owen Melbourne <owen@selesti.com>
2017-01-26 17:56:26 +0000

    unlinking and service stopping added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquire = require('inquirer').createPromptModule();
 const _ = require('underscore');
+require('shelljs/global');
 
 const choices = [
     {name: 'PHP 5.4', value: 'php54'},
@@ -8,26 +9,50 @@ const choices = [
     {name: 'PHP 7.1', value: 'php71'}
 ];
 
+let myChoice = _.first(choices);
+
 const parseAnswers = (answers) => {
 
-    let choice = _.findWhere(choices, {
+    myChoice = _.findWhere(choices, {
         value: answers.version
     });
 
-    if (choice) {
-        deactiveActiveVersion();
-        activateChosenVersion(choice);
+    if (myChoice) {
+        deactiveActiveVersion(activateChosenVersion);
     } else {
         return console.error('No valid version found');
     }
 };
 
-const deactiveActiveVersion = () => {
-    console.log('// To do --- find active version');
+const deactiveActiveVersion = (next) => {
+
+    console.log('\nStopping existing PHP instances, and unlinking symlinks.\n');
+
+    exec('brew services list | grep php', {silent: true, async: false}, function(exitCode, stdout, stderr){
+
+        let potentials = stdout.split('\n');
+
+        _.each(potentials, function (v) {
+
+            let version = v.split(' ');
+
+            if (version[0].indexOf('php') > -1) {
+                version = version[0];
+
+                if (v.indexOf('started') > -1) {
+                    exec('brew services stop ' + version);
+                }
+
+                exec('brew unlink ' + version);
+            }
+        });
+
+        return next();
+    });
 };
 
-const activateChosenVersion = (version) => {
-    console.log('// To do --- activate ' + version.name);
+const activateChosenVersion = () => {
+    console.log('\n// To do --- activate ' + myChoice.name);
 };
 
 inquire([{
