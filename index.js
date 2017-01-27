01ca8dbdcd8b447e6b30fb734c0298481a1a6eb9
Owen Melbourne <owen@selesti.com>
2017-01-27 14:24:28 +0000

    beta ready

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const inquire = require('inquirer').createPromptModule();
 const _ = require('underscore');
+const S = require('cli-spinner').Spinner;
 require('shelljs/global');
 
 const choices = [
     {name: 'PHP 5.4', value: 'php54'},
+    {name: 'PHP 5.5', value: 'php55'},
     {name: 'PHP 5.6', value: 'php56'},
     {name: 'PHP 7', value: 'php70'},
     {name: 'PHP 7.1', value: 'php71'}
 ];
 
 let myChoice = _.first(choices);
+let hasChoiceInstalled = false;
+let spinner = new S('%s ');
+    spinner.setSpinnerString(19);
 
 const parseAnswers = (answers) => {
+
+    spinner.start();
 
     myChoice = _.findWhere(choices, {
         value: answers.version
@@ -26,7 +33,7 @@ const parseAnswers = (answers) => {
 
 const deactiveActiveVersion = (next) => {
 
-    console.log('\nStopping existing PHP instances, and unlinking symlinks.\n');
+    spinner.setSpinnerTitle('Stopping existing PHP instances, and removing symlinks');
 
     exec('brew services list | grep php', {silent: true, async: false}, function(exitCode, stdout, stderr){
 
@@ -39,11 +46,17 @@ const deactiveActiveVersion = (next) => {
             if (version[0].indexOf('php') > -1) {
                 version = version[0];
 
-                if (v.indexOf('started') > -1) {
-                    exec('brew services stop ' + version);
+                if (version == myChoice.value) {
+                    hasChoiceInstalled = true;
                 }
 
-                exec('brew unlink ' + version);
+                if (v.indexOf('started') > -1) {
+                    spinner.setSpinnerTitle('Stopping ' + version);
+                    exec('brew services stop ' + version, {silent: true});
+                }
+
+                spinner.setSpinnerTitle('Unlinking ' + version);
+                exec('brew unlink ' + version, {silent: true});
             }
         });
 
@@ -52,7 +65,29 @@ const deactiveActiveVersion = (next) => {
 };
 
 const activateChosenVersion = () => {
-    console.log('\n// To do --- activate ' + myChoice.name);
+
+    spinner.setSpinnerTitle('Searching for existing installs');
+
+    let activate = function(){
+        exec(`brew link ${myChoice.value} && brew services start ${myChoice.value}`, {silent: true}, function(){
+            spinner.setSpinnerTitle(myChoice.name + ' Activated!');
+
+            setTimeout(function(){
+                spinner.stop();
+                console.log('\n');
+            }, 500);
+        });
+    };
+
+    if (hasChoiceInstalled) {
+        return activate();
+    } else {
+        spinner.setSpinnerTitle(myChoice.name + ' is not installed - installing now (this could take a while)');
+        exec(`brew install ${myChoice.value} ${myChoice.value}-mcrypt ${myChoice.value}-xdebug ${myChoice.value}-imagick ${myChoice.value}-intl`, {silent: true}, function(){
+            spinner.setSpinnerTitle(myChoice.name + ' now installed');
+            activate();
+        });
+    }
 };
 
 inquire([{
